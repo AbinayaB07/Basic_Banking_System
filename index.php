80774cbbdd4a97f49b6c3e37e5cb7bb40b896fbf
Abinaya.B <85489330+AbinayaB07@users.noreply.github.com>
2021-10-20 21:06:17 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+  include 'navbar.php';
+  ?></header>
+<main>
+  <style>
+  body{
+    background-image: url('https://wallpapercave.com/wp/wp6603149.jpg');
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    width: 100%;
+    background-size: cover;
+    background-position: cover;
+
+  }
+  ul{
+    float:right;
+    list-style-type: none;
+    margin-top: 25px;
+  }
+  ul li{
+    display:inline-block;
+  }
+  ul li a{
+    text-decoration: none;
+    color: #fff;
+    padding:5px 20px;
+    border:1px solid #fff;
+  }
+  ul li a:hover{
+    background-color:#fff;
+    color:#000;
+  }
+  }
+
+</style>
+<v1 style = "position:relative; left:70px; top:40px; color:black;  font-family: Comic Sans MS;">W</h1>
+<v1 style = "position:relative; left:0px; top:0px; color:black;  font-family: Comic Sans MS;">E</h1>
+<v1 style = "position:relative; left:0px; top:0px; color:black;  font-family: Comic Sans MS;">L</h1>
+<v1 style = "position:relative; left:0px; top:0px; color:black;  font-family: Comic Sans MS;">C</h1>
+<v1 style = "position:relative; left:0px; top:0px; color:black;  font-family: Comic Sans MS;">O</h1>
+<v1 style = "position:relative; left:0px; top:0px; color:black;  font-family: Comic Sans MS;">M</h1>
+<v1 style = "position:relative; left:0px; top:0px; color:black;  font-family: Comic Sans MS;">E</h1>
+
+<footer class="text-center mt-5 py-2">
+  <p>&copy<b> Made by Abinaya B, Web Development Intern <br> @ Sparks Foundation</p></b>
+<footer>
+    </main>
+  </body>
+</html>
